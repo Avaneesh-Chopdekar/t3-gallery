7d056696c1b907d4bacaf62dcdf3eff920b58f95
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-06-08 18:58:16 +0530

    rate limit implemented

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -87,6 +87,11 @@ export default function SimpleUploadButton() {
         },
       );
     },
+    onUploadError(error) {
+      posthog.capture("upload-error", { error });
+      toast.dismiss("upload-begin");
+      toast.error("Upload failed");
+    },
     onClientUploadComplete: () => {
       toast.dismiss("upload-begin");
       toast("Upload complete!");
